ee7c41018feed354c6b745d2e0716c450a86300f
Ronaldo S.A. Batista <rsilva@anatel.gov.br>
2021-11-16 12:20:15 -0800

    updated course materials

---
+++ setup.sh
@@ -0,0 +1,6 @@
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p $HOME/miniconda
+eval "$(/home/ralves/miniconda/bin/conda shell.bash hook)"
+conda install mamba gh -c conda-forge -y
+gh auth login
+gh repo clone ronaldokun/code-academy
