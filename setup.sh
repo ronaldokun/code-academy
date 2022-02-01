9383ca0d03f1fe9ce619df1281da8510a06aca15
Ronaldo S.A. Batista <rsilva@anatel.gov.br>
2022-01-31 17:17:21 -0800

    Updated setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-bash ~/miniconda.sh -b -p $HOME/miniconda
-eval "$(/home/ralves/miniconda/bin/conda shell.bash hook)"
+bash ~/miniconda.sh -b -p $HOME/miniconda3
+eval "$(/home/ralves/miniconda3/bin/conda shell.bash hook)"
 conda install mamba gh -c conda-forge -y
-gh auth login
-gh repo clone ronaldokun/code-academy
-git config --global user.email "rsilva@anatel.gov.br"
-git config --global user.name "Ronaldo S.A. Batista"
+pip install tensorflow-gpu
+# gh auth login
+# gh repo clone ronaldokun/code-academy
+# git config --global user.email "rsilva@anatel.gov.br"
+# git config --global user.name "Ronaldo S.A. Batista"
