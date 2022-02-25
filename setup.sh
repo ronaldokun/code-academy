8e04ce59639ca03f1bf3b799c92fedfb9468aba1
Ronaldo S.A. Batista <rsilva@anatel.gov.br>
2022-02-25 12:55:52 -0800

    Added "Dive into Deep Learning Notebooks"

--- setup.sh
+++ setup.sh
@@ -2,7 +2,11 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/
 bash ~/miniconda.sh -b -p $HOME/miniconda3
 eval "$(/home/ralves/miniconda3/bin/conda shell.bash hook)"
 conda install mamba gh -c conda-forge -y
-gh auth login
-# gh repo clone ronaldokun/code-academy
+# gh auth login
+gh repo clone ronaldokun/code-academy
+gh repo clone ronaldokun/fsdl
 git config --global user.email "rsilva@anatel.gov.br"
 git config --global user.name "Ronaldo S.A. Batista"
+mamba env create -f code-academy/02-FRAMEWORKS/environment.yml
+workon keras
+
