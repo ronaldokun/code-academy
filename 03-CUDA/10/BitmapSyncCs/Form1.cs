41c2052a375a433c992b53f94afc2241c65b866c
Ronaldo S.A. Batista <rsilva@anatel.gov.br>
2022-04-19 17:41:18 +0000

    📦 Added all code from chapter #6

---
+++ 03-CUDA/10/BitmapSyncCs/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace BitmapSyncCs
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+
+      int dim = 512;
+      var bmp = new Bitmap(dim, dim);
+      var data = bmp.LockBits(new Rectangle(0, 0, dim, dim), ImageLockMode.ReadWrite, bmp.PixelFormat);
+      NativeMethods.GenerateBitmap(data.Scan0, dim);
+      bmp.UnlockBits(data);
+
+      pictureBox.BackgroundImage = bmp;
+    }
+
+        private void pictureBox_Click(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
