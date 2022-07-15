2bf4da3a4eecc33aadbfc6696e61fd7f3e6e9bd1
Ronaldo S.A. Batista <ronaldokun@gmail.com>
2022-07-15 15:08:27 -0300

    Added back notebooks from d2l.ai

--- setup.sh
+++ setup.sh
@@ -1,15 +1,21 @@
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-bash ~/miniconda.sh -b -p $HOME/miniconda3
-eval "$(/home/ralves/miniconda3/bin/conda shell.bash hook)"
-conda install mamba gh -c conda-forge -y
-# gh auth login
-gh repo clone ronaldokun/code-academy
-# gh repo clone ronaldokun/fsdl
-git config --global user.email "rsilva@anatel.gov.br"
-git config --global user.name "Ronaldo S.A. Batista"
-git submodule init
-git submodule update
-mamba env create -f code-academy/02-FRAMEWORKS/environment.yml
-cd code-academy/fsdl && make conda-update && make pip-tools
-mamba clean --all -y
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+# bash ~/miniconda.sh -b -p $HOME/miniconda3
+# eval "$(/home/ralves/miniconda3/bin/conda shell.bash hook)"
+# conda install mamba gh -c conda-forge -y
+# # gh auth login
+# gh repo clone ronaldokun/code-academy
+# # gh repo clone ronaldokun/fsdl
+# git config --global user.email "rsilva@anatel.gov.br"
+# git config --global user.name "Ronaldo S.A. Batista"
+# git submodule init
+# git submodule update
+# mamba env create -f code-academy/02-FRAMEWORKS/environment.yml
+# cd code-academy/fsdl && make conda-update && make pip-tools
+# mamba clean --all -y
+rm -rf d2l
+wget https://d2l.ai/d2l-en.zip
+unzip d2l-en.zip -d d2l
+rm d2l-en.zip
+rm -rf d2l/mxnet
+
 
